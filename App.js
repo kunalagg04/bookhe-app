bd827c101390cbed0b4917ab3b13cf054bc89e3e
KunaL AggarwaL <kunalagg04@gmail.com>
2019-08-16 04:06:36 +0530

    init

---
+++ App.js
@@ -0,0 +1,13 @@
+import { createStackNavigator , createAppContainer } from 'react-navigation';
+import SearchScreen from './src/screens/SearchScreen';
+
+const navigator = createStackNavigator({
+  Search : SearchScreen
+},{
+  initialRouteName: 'Screen',
+  defaultNavigationOptions:{
+    title:'Business Search'
+  }
+});
+
+export default createAppContainer(navigator);
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text , View } from 'react-native'
+
+const SearchScreen = () =>{
+    return(
+        <View>
+            <Text>
+                Restaurant Search
+            </Text>
+        </View>
+    )
+}
+
+export default SearchScreen;
